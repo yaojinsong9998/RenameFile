3e325477e1c47c05dfd136dc2e0d30e52d21352a
yaojinsong <924602776@qq.com>
2022-11-21 00:06:40 +0800

    last

--- RenameFile.py
+++ RenameFile.py
@@ -1,6 +1,6 @@
 import os
 
-path = "D:\\ceshi\\"#文件路径
+path = "/Users/yaojinsong/Desktop/business"#文件路径
 
 originalname = 'Action' #指定字符
 replacename = 'Controller'  #替换字符
@@ -9,14 +9,14 @@ replacename = 'Controller'  #替换字符
 def rename_file(filepath):
     files = os.listdir(filepath)
     for file in files:
-        if os.path.isdir(filepath + '\\' + file):
-            rename_file(filepath + '\\' + file)
+        if os.path.isdir(filepath + '/' + file):
+            rename_file(filepath + '/' + file)
         else:
-            subFiles = os.listdir(filepath + '\\')
+            subFiles = os.listdir(filepath + '/')
             for subFile in subFiles:
                 if originalname in subFile:
-                    n = str(filepath + '\\' + subFile.replace(originalname, replacename))
-                    n1 = str(filepath + '\\' + str(subFile))
+                    n = str(filepath + '/' + subFile.replace(originalname, replacename))
+                    n1 = str(filepath + '/' + str(subFile))
                     try:
                         os.rename(n1, n)
                     except IOError:
